c7dd81dc121c5c00e9b64188bd88a36c0eeb4fad
Caro <55109108+CaroMen@users.noreply.github.com>
2021-01-07 19:40:43 -0600

    more of welcome screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import AppButton from '../components/AppButton';
 
 function WelcomeScreen(props) {
     return (
         <ImageBackground
+            blurRadius={10}
             style={styles.background}
             source={require('../assets/background.jpg')}>
             <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require('../assets/logo-red.png')} />
-                <Text>Sell What You Don't Need</Text>
+                <Text style={styles.tagline}>Sell What You Don't Need</Text>
             </View>
-            <View style={styles.loginButton}></View>
-            <View style={styles.registerButton}></View>
+            <View style={styles.buttonsContainer}>
+                <AppButton title="login" />
+                <AppButton title="register" color="secondary" />
+            </View>
         </ImageBackground>
     );
 }
@@ -22,15 +26,9 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: 'center',
     },
-    loginButton: {
+    buttonsContainer: {
+        padding: 20,
         width: '100%',
-        height: 70,
-        backgroundColor: '#fc5c65',
-    },
-    registerButton: {
-        width: '100%',
-        height: 70,
-        backgroundColor: '#4ecdc4',
     },
     logo: {
         width: 100,
@@ -40,6 +38,11 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: 70,
         alignItems: 'center'
+    },
+    tagline: {
+        fontSize: 25,
+        fontWeight: '600',
+        paddingVertical: 20,
     }
 })
 
